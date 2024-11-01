2de9542c1f5c6d081cadf793aee3993e33b1bfe5
Ziagl <office@ziegelwanger-edv.at>
2024-11-01 14:12:19 +0100

    version 0.0.4

---
+++ HexMapUnits/Models/UnitType.cs
@@ -0,0 +1,21 @@
+﻿namespace HexMapUnits.Models;
+
+public record UnitType
+{
+    // base
+    public string Name = string.Empty; // the name of this unit type
+    public string Description = string.Empty; // short description for what this unit is used
+    public int Type; // the type of this unit (value of an enum?)
+    public int Era; // min era for this unit
+    public int MaxHealth; // maximum health points
+    public int MaxMovement; // maximum movement points (affects movement range)
+    // stats
+    public int Attack; // attack points (damage in fight)
+    public int Defense; // defence points (how much damage is reduced)
+    public int Range; // attack range (how far can this unit attack)
+    // flags
+    public bool CanAttack; // can this unit attack?
+    // economy
+    public int ProductionCost; // amount of production needed to build this unit
+    public int PurchaseCost; // amount of gold needed to purchase this unit
+}
