e73273c8b4ec1033d342cfb830672106857c8788
Ziagl <office@ziegelwanger-edv.at>
2025-03-21 16:07:06 +0100

    version 0.3.3

--- HexMapUnits/Models/UnitBase.cs
+++ HexMapUnits/Models/UnitBase.cs
@@ -1,33 +1,38 @@
-﻿using com.hexagonsimulations.Geometry.Hex.Interfaces;
+﻿using com.hexagonsimulations.HexMapBase.Interfaces;
 using com.hexagonsimulations.HexMapBase.Models;
 
 namespace com.hexagonsimulations.HexMapUnits.Models;
 
-public class UnitBase : GameEntity
+public class UnitBase : IBaseEntity, ICombatEntity
 {
-    // base
-    public string Name = string.Empty; // the name of this unit type
-    public List<string> Images = new(); //representation of this unit in UI and/or map
-    public string Description = string.Empty; // short description for what this unit is used
-    public int Type; // the type of this unit (value of an enum?)
-    public int Era; // min era for this unit
-    public int MaxMovement; // maximum movement points (affects movement range)
-    public int MovementType; // type of unit (f.e. move, wheel, shallow water, deep water, air, ...)
-    public int Movement; // current movement points
-    // stats
-    public int WeaponType; // type of weapon/combat of this unit (infantry, cavalry, ...)
-    public int Attack; // attack points (damage in fight)
-    public int RangedAttack; // ranged attack points (damage of airstrike)
-    public int Defense; // defence points (how much damage is reduced)
-    public int Range; // attack range (how far can this unit attack)
+    // base entity
+    public int Id { get; set; }
+    public int Player { get; set; }
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
+    // base unit
+    public string Name { get; set; } = string.Empty; // the name of this unit type
+    public List<string> Images { get; set; } = new(); //representation of this unit in UI and/or map
+    public string Description { get; set; } = string.Empty; // short description for what this unit is used
+    public int Type { get; set; } // the type of this unit (value of an enum?)
+    public int Era { get; set; } // min era for this unit
+    public int MaxMovement { get; set; } // maximum movement points (affects movement range)
+    public int MovementType { get; set; } // type of unit (f.e. move, wheel, shallow water, deep water, air, ...)
+    public int Movement { get; set; } // current movement points
+    // combat
+    public int WeaponType { get; set; } // type of weapon/combat of this unit (infantry, cavalry, ...)
+    public int Attack { get; set; } // attack points (damage in fight)
+    public int RangedAttack { get; set; } // ranged attack points (damage of airstrike)
+    public int Defense { get; set; } // defence points (how much damage is reduced)
+    public int Range { get; set; } // attack range (how far can this unit attack)
     // flags
-    public bool CanAttack; // can this unit attack?
-    public bool CanBuildCity; // can this unit build a new city?
+    public bool CanAttack { get; set; } // can this unit attack?
+    public bool CanBuildCity { get; set; } // can this unit build a new city?
     // economy
-    public Dictionary<int, int> Goods = new(); // goods needed to produce this unit
-    public int ProductionCost; // amount of production needed to build this unit
-    public int PurchaseCost; // amount of gold needed to purchase this unit
+    public Dictionary<int, int> Goods { get; set; } = new(); // goods needed to produce this unit
+    public int ProductionCost { get; set; } // amount of production needed to build this unit
+    public int PurchaseCost { get; set; } // amount of gold needed to purchase this unit
     // position
-    public CubeCoordinates Position; // its position on the map
-    public int Layer; // index of layer this unit is on
+    public CubeCoordinates Position { get; set; } // its position on the map
+    public int Layer { get; set; } // index of layer this unit is on
 }
